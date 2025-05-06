a5c44a08a15196baedc6339925059bec6b6fc864
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-07 00:38:42 +0700

    Create program.js

---
+++ sanity/schemas/program.js
@@ -0,0 +1,48 @@
+export default {
+  name: 'program',
+  title: 'Program',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      rows: 3
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'status',
+      title: 'Status (e.g., Coming Soon, Active)',
+      type: 'string'
+    },
+    {
+      name: 'order',
+      title: 'Order (for sorting)',
+      type: 'number',
+      initialValue: 0
+    }
+  ]
+}
