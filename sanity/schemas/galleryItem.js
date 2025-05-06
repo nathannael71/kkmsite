138c9a445fd23387293abe9197fc61a2fd6980e7
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-07 00:42:44 +0700

    Create galleryItem.js

---
+++ sanity/schemas/galleryItem.js
@@ -0,0 +1,49 @@
+export default {
+  name: 'galleryItem',
+  title: 'Gallery Item',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      rows: 3
+    },
+    {
+      name: 'date',
+      title: 'Date (e.g., March 2024)',
+      type: 'string'
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}]
+    }
+  ]
+}
